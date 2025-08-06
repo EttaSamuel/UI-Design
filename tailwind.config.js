fb776d809675984d1ceb312fad236f0a62c1ca23
EttaSamuel <ettasamuel8@gmail.com>
2025-08-06 21:45:45 +0100

    Initial commit of Tailwind CSS UI Design Practice  project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html',"./node_modules/flowbite/**/*.js"],
+  theme: {
+    screens:{
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
+    extend: {
+      backgroundColor: ['active'],
+      
+      colors:{
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        workPayLight: '#A0BACC',
+        workPayBlue: '#08546c'
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
